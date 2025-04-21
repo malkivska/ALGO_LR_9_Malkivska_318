f7c4b8f0d3c8f0db705e0e29f0a6a1cbf3fca003
malkivska <v.a.malkivska@student.khai.edu>
2025-04-21 16:10:14 +0300

    task1.h

---
+++ task1.h
@@ -0,0 +1,8 @@
+#ifndef TASK1_H  // Перевірка, чи не була вже включена ця бібліотека
+#define TASK1_H  // Якщо не була, визначаємо її
+
+// Оголошення функції customReplace, яка реалізує заміну частини рядка
+void customReplace(char* source, size_t pos, size_t len, const char* insert, size_t subpos, size_t sublen);
+
+// Завершуємо захист від подвійного включення
+#endif // TASK1_H
